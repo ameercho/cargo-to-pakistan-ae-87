46af29e11fbd4a1cb33d1484e7f30240ae0f58e9
ameercho <amirshoaibch@gmail.com>
2025-05-21 09:15:58 +0000

    a

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,6 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: [
-      "76e261ed-2b68-4ea6-bb4b-42b9110ee4b3.lovableproject.com",
-      "76e261ed-2b68-4ea6-bb4b-42b9110ee4b3.lovable.app",
-      "id-preview--76e261ed-2b68-4ea6-bb4b-42b9110ee4b3.lovable.app"
-    ]
   },
   plugins: [
     react(),
